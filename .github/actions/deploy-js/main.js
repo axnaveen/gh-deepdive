e8e485007f6c9bce69dc9355d1b487f52d452afe
axnaveen <45172627+axnaveen@users.noreply.github.com>
2024-08-08 17:50:02 +0000

    JavaScript Input

--- .github/actions/deploy-js/main.js
+++ .github/actions/deploy-js/main.js
@@ -2,7 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run(){
+    const bucket_name = core.getInput(bucket, {required: true});
+    const bucket_region = core.getInput(bucket-region, {required: true});
     core.notice("Hello from Java Script");
+    core.notice(bucket_name, bucket_region);
 }
 
 run();
