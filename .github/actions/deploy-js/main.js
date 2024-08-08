cc788d2fbaede27af347cd04595847cb63d0aad0
axnaveen <45172627+axnaveen@users.noreply.github.com>
2024-08-08 17:57:17 +0000

    Added JS custom action output

--- .github/actions/deploy-js/main.js
+++ .github/actions/deploy-js/main.js
@@ -4,8 +4,11 @@ const exec = require('@actions/exec');
 function run(){
     const bucket_name = core.getInput('bucket', {required: true});
     const bucket_region = core.getInput('bucket-region', {required: true});
+
+    const bucket_url = `http://${bucket_name}/hi`
     core.notice("Hello from Java Script");
     core.notice(bucket_name, bucket_region);
+    core.setOutput('bucket-url',bucket_url)
 }
 
 run();
